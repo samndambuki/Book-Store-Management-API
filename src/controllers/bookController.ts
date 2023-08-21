1e6ff0fb66d0798ac9f4d6aba037ac2dc1254b8c
samndambuki <sam.ndambuki08@gmail.com>
2023-08-21 07:49:37 +0300

    commented bookRoutes

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,10 +1,9 @@
 // controllers/bookController.ts
-import { Request, Response } from 'express';
-import pool from '../db';
-import { Book } from '../models/Book';
+import { Request, Response, query } from "express";
+import pool from "../db";
+import { Book } from "../models/Book";
 
 // Create a new book
-
 
 //defines an exported function that's asynchronous
 //takes two parameters, request, response
@@ -19,30 +18,42 @@ export const createBook = async (req: Request, res: Response) => {
 
     //send the sql query to database
     //result of the query is stored in result variable
-    const result = await pool.request()
-      .input('title', title)
-      .input('author', author)
+    const result = await pool
+      .request()
+      .input("title", title)
+      .input("author", author)
       .query(query);
 
-      //201  - successful creation
-    res.status(201).json({ message: 'Book created successfully' });
+    //201  - successful creation
+    res.status(201).json({ message: "Book created successfully" });
   } catch (error) {
-    console.error('Error creating book:', error);
+    console.error("Error creating book:", error);
     //500 - Internal server error
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
 // Get all books
+//takes to arguments, req: incoming request object, res:outgoinh response object
 export const getAllBooks = async (req: Request, res: Response) => {
+  //logic for fetching and sending books
   try {
+    //query to select all columns from books table
     const query = `SELECT * FROM books`;
+    //await - operation should be awaited since its asychronous
+    //creates a request to database using connection pool
+    //result fo the query is stored in a result variable
     const result = await pool.request().query(query);
+    //recrdset - contains the rows returned by sql query
+    //type annotation Book[] indicatses books variable is expected to be an array of objects that match the Book type
     const books: Book[] = result.recordset;
 
+    //200 - OK
     res.status(200).json(books);
   } catch (error) {
-    console.error('Error fetching books:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    //if an error occurs inside the try block, the cod einside the catch blick will execute
+    console.error("Error fetching books:", error);
+    //500 - internal server error
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,13 +1,22 @@
-// routes/bookRoutes.ts
+// routes/bookRoutes.ts - sets up routes related to books
 import express from 'express';
 import { createBook, getAllBooks } from '../controllers/bookController';
 
+//create an instance of the express router
+//router object will be used to define routes for the application
 const router = express.Router();
 
 // Route to create a new book
+
+//define a POST route at the path '/books' using router.POST method
+//when a POST request is made, createBook function will be invoked
 router.post('/books', createBook);
 
 // Route to get all books
+
+//we define a GET route at the path /books using router.get() method
+//when a GET request is made to this endpount, getAllBooks function will be invoked to hanlde the request
 router.get('/books', getAllBooks);
 
+//export the router object so that it can be imported and used in other parts of the application
 export default router;
